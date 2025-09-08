212f438d86352beda4dd61220ccf3756ec3a75d3
Murugo <murugalstudio@gmail.com>
2025-09-08 01:01:03 -0700

    Prevent morph vertices from resetting for some animations

--- VLR.Fixes/LipAnim.cpp
+++ VLR.Fixes/LipAnim.cpp
@@ -15,6 +15,8 @@ static VlrInsertValueAtFunc VlrInsertValueAt;
 static BYTE* jmpUpdateStartKeyframesReturnAddr = nullptr;
 static BYTE* jmpUpdateEndKeyframesReturnAddr = nullptr;
 static BYTE* jmpClampPhonemeStartTimeReturnAddr = nullptr;
+static BYTE* jmpSkipMorphResetReturnAddr1 = nullptr;
+static BYTE* jmpSkipMorphResetReturnAddr2 = nullptr;
 
 struct KeyframeList
 {
@@ -119,7 +121,7 @@ __declspec(naked) void __stdcall UpdateLipEndKeyframesASM()
 
 constexpr float kPhonemeStartTime = 0.001f;
 
-// Clamp start time used for phonemes to prevent negative time values.
+// Clamps start time used for phonemes to prevent negative time values.
 __declspec(naked) void __stdcall ClampPhonemeStartTimeASM()
 {
     __asm
@@ -135,6 +137,50 @@ __declspec(naked) void __stdcall ClampPhonemeStartTimeASM()
 
     ExitASM:
         jmp jmpClampPhonemeStartTimeReturnAddr
+    }
+}
+
+// Skips resetting morph target vertices of an active animation after
+// the animation has already been initialized. This fix should only
+// apply to animations where the character has both eyes closed.
+//
+// Some characters (e.g. Quark, Clover) are susceptible to a bug where
+// if they have their eyes closed, and the blink animation plays while
+// they are talking, their mouth shape will become corrupted. This is a
+// special case where the blink itself inadvertently triggers all face
+// vertices to reset. This issue is much more apparent with the lip
+// animation fix enabled, hence the two fixes are bundled together.
+__declspec(naked) void __stdcall SkipMorphResetASM()
+{
+    __asm
+    {
+        cmp esi, 0x7
+        jne ResetOk
+
+        mov esi, dword ptr ds : [esp + 0x14]
+        mov ecx, dword ptr ds : [esp + 0x20]
+        push eax
+        mov eax, dword ptr ds : [esi + 0x2C]
+        mov eax, dword ptr ds : [eax + ecx * 0x04]
+        test eax, eax
+        pop eax
+        jnz SkipReset
+
+        push eax
+        mov eax, dword ptr ds : [esi + 0x18]
+        mov eax, dword ptr ds : [eax + ecx * 0x04]
+        cmp eax, 0x3F800000
+        pop eax
+        jnz SkipReset
+
+    ResetOk:
+        mov eax, [ebx]
+        xor ecx, ecx
+        mov dword ptr ds : [esp + 0x24], ecx
+        jmp jmpSkipMorphResetReturnAddr1
+
+    SkipReset:
+        jmp jmpSkipMorphResetReturnAddr2
     }
 }
 
@@ -157,21 +203,31 @@ bool PatchLipAnimationFix()
     auto clamp_phoneme_pattern = hook::pattern("F3 0F 11 45 FC FF 77 08");
     if (clamp_phoneme_pattern.size() != 1)
     {
-        LOG(LOG_ERROR) << __FUNCTION__ << ": Failed to find memory address for phoneme start time";
+        LOG(LOG_ERROR) << __FUNCTION__ << ": Failed to find memory address for phoneme start time fix";
+        return false;
+    }
+    auto skip_morph_reset_pattern = hook::pattern("8B 03 33 C9 89 4C 24 24");
+    if (skip_morph_reset_pattern.size() != 1)
+    {
+        LOG(LOG_ERROR) << __FUNCTION__ << ": Failed to find memory address for skip morph reset fix";
         return false;
     }
     BYTE* start_kf_inject_addr = start_kf_pattern.count(1).get(0).get<BYTE>(0);
     BYTE* end_kf_inject_addr = end_kf_pattern.count(1).get(0).get<BYTE>(0);
     BYTE* clamp_phoneme_inject_addr = clamp_phoneme_pattern.count(1).get(0).get<BYTE>(0);
+    BYTE* skip_morph_reset_addr = skip_morph_reset_pattern.count(1).get(0).get<BYTE>(0);
     VlrInsertValueAt = (VlrInsertValueAtFunc)(*(DWORD*)(end_kf_inject_addr - 0x19) + end_kf_inject_addr - 0x15);
     jmpUpdateStartKeyframesReturnAddr = start_kf_inject_addr + 7;
     jmpUpdateEndKeyframesReturnAddr = end_kf_inject_addr + 6;
     jmpClampPhonemeStartTimeReturnAddr = clamp_phoneme_inject_addr + 5;
+    jmpSkipMorphResetReturnAddr1 = skip_morph_reset_addr + 8;
+    jmpSkipMorphResetReturnAddr2 = skip_morph_reset_addr + 0xDB;
 
     LOG(LOG_INFO) << "Patching lip animation fix...";
     WriteJmp(start_kf_inject_addr, UpdateLipStartKeyframesASM, 7);
     WriteJmp(end_kf_inject_addr, UpdateLipEndKeyframesASM, 6);
     WriteJmp(clamp_phoneme_inject_addr, ClampPhonemeStartTimeASM, 5);
+    WriteJmp(skip_morph_reset_addr, SkipMorphResetASM, 8);
     return true;
 }
 
