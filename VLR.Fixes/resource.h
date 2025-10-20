ddb13267183cdf5f646d223f44c79b5c162feae0
Murugo <murugalstudio@gmail.com>
2025-10-19 17:02:36 -0700

    Update minor version

--- VLR.Fixes/resource.h
+++ VLR.Fixes/resource.h
@@ -17,7 +17,7 @@
 #define STR(x) STR_HELPER(x)
 
 #define VERSION_MAJOR               1
-#define VERSION_MINOR               1
+#define VERSION_MINOR               2
 #define VERSION_REVISION            0
 #define VERSION_BUILD               0
 
